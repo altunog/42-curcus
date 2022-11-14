2ee43ef772e2ea0a6770380d259ea413d52454c1
altunog <altunog@gmail.com>
2022-11-14 11:41:57 +0300

    initial commit

---
+++ libft/libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oaltun <oaltun@student.42istanbul.com.tr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/14 10:39:21 by oaltun            #+#    #+#             */
+/*   Updated: 2022/11/14 10:39:22 by oaltun           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int ft_isalnum(int c);
+int	ft_isascii(int c);
+
+
+#endif
