44d46dbacead3670ef7239318bbc40afbefe92e8
altunog <altunog@gmail.com>
2022-11-15 00:20:15 +0300

    update libft.h, add ft_bzero.c

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -1,29 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: oaltun <oaltun@student.42istanbul.com.tr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/14 10:39:21 by oaltun            #+#    #+#             */
-/*   Updated: 2022/11/14 10:39:22 by oaltun           ###   ########.fr       */
+/*   Created: 2022/11/14 20:24:04 by oaltun            #+#    #+#             */
+/*   Updated: 2022/11/14 20:24:09 by oaltun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-#include <unistd.h>
-#include <stdlib.h>
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-void	*ft_memset(void *b, int c, size_t len);
-void	bzero(void *s, size_t n);
-
-#endif
+void	ft_bzero(void *s, size_t n)
+{
+	ft_memset(s, 0, n);
+}
