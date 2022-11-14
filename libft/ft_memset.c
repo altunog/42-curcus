25e6c2f8c56b21a8171660ec715e057af5078e7d
altunog <altunog@gmail.com>
2022-11-14 19:58:35 +0300

    add memset, update .gitignore

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -1,28 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: oaltun <oaltun@student.42istanbul.com.tr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/14 10:39:21 by oaltun            #+#    #+#             */
-/*   Updated: 2022/11/14 10:39:22 by oaltun           ###   ########.fr       */
+/*   Created: 2022/11/14 14:09:01 by oaltun            #+#    #+#             */
+/*   Updated: 2022/11/14 18:42:51 by oaltun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-#include <unistd.h>
-#include <stdlib.h>
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char	*dst;
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-void	*ft_memset(void *b, int c, size_t len);
-
-#endif
+	dst = b;
+	while (len > 0)
+	{
+		*dst++ = c;
+		len--;
+	}
+	return (b);
+}
