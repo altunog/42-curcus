0c50543c1bfd8f7b2730e08e6a6519f0e008fb07
altunog <altunog@gmail.com>
2022-11-16 14:10:50 +0300

    add ft_strncmp

---
+++ libft/ft_strncmp.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oaltun <oaltun@student.42istanbul.com.tr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/16 13:42:17 by oaltun            #+#    #+#             */
+/*   Updated: 2022/11/16 13:42:18 by oaltun           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	int	comparison;
+
+	comparison = 0;
+	while (n-- && *s1 && *s2)
+	{
+		if (*s1 != *s2)
+		{
+			comparison = *s1 - *s2;
+			break;
+		}
+		s1++;
+		s2++;
+	}
+	return (comparison);
+}
+#include <stdio.h>
+#include <string.h>
+int main() {
+	char s1[] = "";
+	char s2[] = "";
+
+	printf("ft -> %d\n", ft_strncmp(s1, s2, sizeof(s1)));
+	printf("og -> %d\n", strncmp(s1, s2, sizeof(s1)));
+}
