aa2cc21ac53551ee7411bc03a1507fdbca051c56
altunog <altunog@gmail.com>
2022-11-18 11:39:23 +0300

    add ft_calloc

---
+++ libft/ft_calloc.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oaltun <oaltun@student.42istanbul.com.tr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/18 10:51:53 by oaltun            #+#    #+#             */
+/*   Updated: 2022/11/18 10:51:54 by oaltun           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	char	*mem;
+
+	mem = malloc(count * size);
+	if (!mem)
+		return (NULL);
+	ft_bzero((void *)mem, count * size);
+
+	return ((void *)mem);
+}
