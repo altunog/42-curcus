974c5216bed51b910eeae1165d5853548989f7da
altunog <altunog@gmail.com>
2022-11-21 17:22:03 +0300

    add ft_lstadd_back

---
+++ libft/ft_lstadd_back.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oaltun <oaltun@student.42istanbul.com.tr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 16:19:05 by oaltun            #+#    #+#             */
+/*   Updated: 2022/11/21 16:19:06 by oaltun           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*current;
+
+	if (!new)
+		return ;
+	if (!*lst)
+	{
+		*lst = new;
+		return;
+	}
+	current = ft_lstlast(*lst);
+	current->next = new;
+}
