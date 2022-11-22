e681b9e6aa5ad349f8d12ccfe9ab087b72f75aea
altunog <altunog@gmail.com>
2022-11-22 13:04:48 +0300

    add ft_lstmap, update ft_lstdelone

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -1,20 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: oaltun <oaltun@student.42istanbul.com.tr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/21 17:27:22 by oaltun            #+#    #+#             */
-/*   Updated: 2022/11/21 17:27:26 by oaltun           ###   ########.fr       */
+/*   Created: 2022/11/22 11:37:46 by oaltun            #+#    #+#             */
+/*   Updated: 2022/11/22 11:37:47 by oaltun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstdelone(t_list *lst, void (*del)(void*))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	if (!lst || !del)
-		return ;
-	del(lst->content);
+	t_list	*node_mapped;
+	t_list	*new_lst;
+
+	if (!lst || (!f && !del))
+		return (NULL);
+	new_lst = NULL;
+	while (lst)
+	{
+		node_mapped = ft_lstnew(f(lst->content));
+		if (!node_mapped)
+		{
+			ft_lstclear(&new_lst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_lst, node_mapped);
+		lst = lst->next;
+	}
+	return (new_lst);
 }
