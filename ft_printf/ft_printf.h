83d8abec5a30d747d6e44038128bcf04d27cc998
altunog <altunog@gmail.com>
2022-11-28 16:23:22 +0300

    add ft_printf.h

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oaltun <oaltun@student.42istanbul.com.tr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/28 12:48:49 by oaltun            #+#    #+#             */
+/*   Updated: 2022/11/28 12:48:50 by oaltun           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF
+# define FT_PRINTF
+# include <unistd.h>
+# include <stdlib.h>
+
+int	ft_putchar(int c);
+int	ft_putstr(const char *s);
+int	ft_putptr(void *p);
+int	ft_putnbr_d(int n);
+int	ft_putnbr_i(int n);
+int	ft_putnbr_u(unsigned int n);
+int	ft_putnbr_x(int n);
+int	ft_printf(const char *format, ...);
+int	ft_specify(char *str, char specifier);
+
+#endif
